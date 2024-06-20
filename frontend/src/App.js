facc34b2004084c056856453ce0fa5ab321afe89
Mayank <2020kucp1006@iiitkota.ac.in>
2024-06-20 11:21:02 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,7 @@ import Login from './Components/Login/Login';
 
 import './App.css';
 import AboutPage from './Components/Pages/AboutPage';
-<<<<<<< HEAD
-import ServicePage from './Components/Pages/ServicePage';
-import TermCondition from './Components/Pages/TermCondition';
-import ContactPage from './Components/Pages/ContactPage'
-=======
 import Gallery from './Components/Pages/Gallery-page/Gallery';
->>>>>>> 468a4261d271a309c1a2016c305a97eb26b93589
 
 
 const App = () => {
@@ -43,18 +37,6 @@ const App = () => {
         <Route
           exact path="/about-page"
           element={<AboutPage />}
-        />
-       <Route
-          exact path="/contact-page"
-          element={<ContactPage />}
-        />
-       <Route
-          exact path="/service-page"
-          element={<ServicePage />}
-        />
-        <Route
-        exact path="/term&condition-page"
-        element={<TermCondition/>}
         />
 
         <Route
