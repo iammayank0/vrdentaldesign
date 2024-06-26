221e7a2d48b4ad07b44c7cca6d452627291a20b8
Mayank <2020kucp1006@iiitkota.ac.in>
2024-06-26 14:56:44 +0530

    banner

--- frontend/src/Components/Main/Home-Page/Banner.js
+++ frontend/src/Components/Main/Home-Page/Banner.js
@@ -4,15 +4,19 @@ import './Banner.css';
 import Navbar from '../../Navbar/Navbar';
 
 const Banner = () => {
+  const handleVideoError = (e) => {
+    console.error("Error loading video", e);
+  };
+
   return (
     <div className="banner-container">
-        <Navbar/>
-      <video className="video" autoPlay loop muted playsInline>
+        <Navbar />
+      <video className="video" autoPlay loop muted playsInline onError={handleVideoError}>
         <source src={banner} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
